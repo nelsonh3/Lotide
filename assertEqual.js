e2286bac4f560d937e4ec4771e4595118c92b33d
Nelson Huang <n-huang@live.com>
2021-05-25 01:45:06 +0000

    lotide

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`)
+    
+  } else (actual !== expected) {
+
+    console.log(` Assertion Failed: ${actual} !== ${expected}`)
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
