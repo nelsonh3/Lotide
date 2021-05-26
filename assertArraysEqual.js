703de380f800f23d2b6712ef2d15e7f5cdfa70dd
Nelson Huang <n-huang@live.com>
2021-05-26 01:50:44 +0000

    lotide

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+
+const eqArrays = function(arr1, arr2){
+  if(arr1.length !== arr2.length){
+    return false
+  }else{
+    let result = false
+  }
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      return false
+    }else{
+       result = true
+    }
+  }return result
+}
+
+
+const assertArraysEqual = function(arr1, arr2){
+  const result = eqArrays(arr1, arr2)
+  if(result) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+  else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  
+  }
+
+}
+
